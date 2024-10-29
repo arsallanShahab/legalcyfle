ef24ed40752ed333182808632155a1d89457903c
Arsallan Shahab <mdarsallan@gmail.com>
2024-10-29 15:54:16 +0530

    adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <Providers>
       <Script
         id="adsbygoogle-init"
-        strategy="afterInteractive"
         crossOrigin="anonymous"
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5892936530350741"
       />
